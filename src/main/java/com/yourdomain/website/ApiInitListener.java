0833bf5e68e4ab018e1feefe9bd5d42d293c0d01
Eiffel Q <eiffelqiu@gmail.com>
2011-09-09 11:25:52 +0800

    add source code

---
+++ src/main/java/com/yourdomain/website/ApiInitListener.java
@@ -0,0 +1,21 @@
+package com.yourdomain.website;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import com.renren.api.client.RenrenApiConfig;
+import com.yourdomain.website.config.AppConfig;
+
+public class ApiInitListener implements ServletContextListener{
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		//Nothing to do
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		RenrenApiConfig.renrenApiKey = AppConfig.API_KEY;
+		RenrenApiConfig.renrenApiSecret = AppConfig.APP_SECRET;
+	}
+}
